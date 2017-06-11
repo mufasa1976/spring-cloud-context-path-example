48147b7cfcbb166a2d221465773f7412815e6de6
mufasa1976 <mufasa1976@coolstuff.software>
2017-06-11 11:15:35 +0200

    added a Fallback Szenario

--- client-boot/src/main/java/io/github/mufasa1976/spring/cloud/example2/client/remote/HeroApiFeignClientFallback.java
+++ client-boot/src/main/java/io/github/mufasa1976/spring/cloud/example2/client/remote/HeroApiFeignClientFallback.java
@@ -1,23 +1,36 @@
 package io.github.mufasa1976.spring.cloud.example2.client.remote;
 
-import io.github.mufasa1976.spring.cloud.example2.common.Hero;
-import org.springframework.stereotype.Component;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+import io.github.mufasa1976.spring.cloud.example2.common.Hero;
 
 @Component
 public class HeroApiFeignClientFallback implements HeroApiFeignClient {
 
+  private final static Hero FALLBACK_HERO = Hero.builder()
+      .reference("000000")
+      .name("Fallback Hero")
+      .build();
+  private final static List<Hero> FALLBACK_HEROES = new ArrayList<>();
+
+  @PostConstruct
+  public void initialize() {
+    FALLBACK_HEROES.add(FALLBACK_HERO);
+  }
+
   @Override
   public List<Hero> findAll() {
-    return new ArrayList<>();
+    return FALLBACK_HEROES;
   }
 
   @Override
   public Hero find(String reference) {
-    return null;
+    return FALLBACK_HERO;
   }
 
 }
